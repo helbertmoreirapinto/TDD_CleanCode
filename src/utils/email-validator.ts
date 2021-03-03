4ba8e5ba949dbff29aafae5c352b7f49777056b8
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-03 08:56:51 -0300

    feat: ensure EmailValidatorAdapter return true if validator returns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,6 +1,8 @@
 import { EmailValidator } from '../presentation/controllers/singup/protocols/emailValidator'
+import validator from 'validator'
+
 export class EmailValidatorAdapter implements EmailValidator {
   async isValid (email: string): Promise<boolean> {
-    return await new Promise(resolve => resolve(false))
+    return await new Promise(resolve => resolve(validator.isEmail(email)))
   }
 }
