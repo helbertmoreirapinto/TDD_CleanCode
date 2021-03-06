d033f71cce98857ba8452da2eab56dbc652d91b7
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-06 17:54:00 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,5 @@
-import express from 'express'
+import app from './config/app'
 
-const app = express()
 const port = process.env.PORT || 3333
+
 app.listen(port, () => console.log(`Server running on port http://localhost:${port}`))
