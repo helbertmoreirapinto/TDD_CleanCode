21a38f03797b060e06a6e98386d68f3d9f99546f
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-08 14:59:04 -0300

    feat: ensure LogMongoRepository creates an error log on success

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -2,6 +2,7 @@ import { SingUpController } from '../../presentation/controllers/singup/singup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { BCryptAdapter } from '../../infra/criptografy/bcrypt-adapter'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
@@ -16,5 +17,7 @@ export const makeSingupController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
 
   const singupController = new SingUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(singupController)
+
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(singupController, logMongoRepository)
 }
