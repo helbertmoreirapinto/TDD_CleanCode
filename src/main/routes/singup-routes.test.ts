0453042bc640c8810bfff96e2ff300922f8dfa84
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-08 09:24:21 -0300

    refactor: connect to mongodb before tests

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
 
 describe('Singup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCoollection = MongoHelper.getCollection('accounts')
+    await accountCoollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/singup')
