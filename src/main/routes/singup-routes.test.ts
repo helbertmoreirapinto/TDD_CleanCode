c3488f6d4557d09836684bf35d8b5401b0af6aa7
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-08 09:16:15 -0300

    test: ensure singup routes return an account on success

---
+++ src/main/routes/singup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Singup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/singup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+
+      })
+      .expect(200)
+  })
+})
