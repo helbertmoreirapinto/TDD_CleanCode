a496196caec40a0fad38e54534efa0cc76a65e67
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-08 12:22:26 -0300

    feat: ensure LogControllerDecorator calls controller with correct values

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -3,8 +3,10 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { BCryptAdapter } from '../../infra/criptografy/bcrypt-adapter'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSingupController = (): SingUpController => {
+export const makeSingupController = (): Controller => {
   const salt = 12
   const bCryptAdapter = new BCryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
@@ -13,5 +15,6 @@ export const makeSingupController = (): SingUpController => {
 
   const emailValidatorAdapter = new EmailValidatorAdapter()
 
-  return new SingUpController(emailValidatorAdapter, dbAddAccount)
+  const singupController = new SingUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(singupController)
 }
--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -38,7 +38,6 @@ export class SingUpController implements Controller {
 
       return ok(account)
     } catch (error) {
-      console.error(error)
       return internalServerError()
     }
   }
