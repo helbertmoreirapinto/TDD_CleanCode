e47d78c161e848e4f716ed5c47e665fc0e28a2a0
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-09 20:06:14 -0300

    feat: ensure DbAuthenticator calls UpdateAccessToken with correct values

--- src/data/usecases/authenticator/db-authenticator.ts
+++ src/data/usecases/authenticator/db-authenticator.ts
@@ -1,18 +1,21 @@
-import { Authenticator, AuthenticatorModel, LoadAccountByEmailRepository, HashComparer, TokenGenerator } from './db-authenticator-protocols'
+import { Authenticator, AuthenticatorModel, LoadAccountByEmailRepository, HashComparer, TokenGenerator, UpdateAccessTokenRepository } from './db-authenticator-protocols'
 
 export class DbAuthenticator implements Authenticator {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator
+    tokenGenerator: TokenGenerator,
+    updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (authData: AuthenticatorModel): Promise<string> {
@@ -20,7 +23,9 @@ export class DbAuthenticator implements Authenticator {
     if (account) {
       const compare = await this.hashComparer.compare(authData.password, account.password)
       if (compare) {
-        return await this.tokenGenerator.generate(account.id)
+        const token = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, token)
+        return token
       }
     }
     return null
