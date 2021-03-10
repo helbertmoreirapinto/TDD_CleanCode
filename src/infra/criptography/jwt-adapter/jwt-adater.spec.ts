399db97075a634c56a774249d5344995a2bc1f6f
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-10 07:33:58 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adater.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adater.spec.ts
@@ -33,4 +33,13 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  test('Should return access token on sign success', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const accessTokenPromise = sut.encrypt('any_id')
+    await expect(accessTokenPromise).rejects.toThrow()
+  })
 })
