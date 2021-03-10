5cbb82e45aa3f0c229561ca6eaa6af1f18c233de
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-10 13:28:19 -0300

    refactor: fix imports error

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 3333,
-  jwtSecret: process.env.JET_SECRET || '4ihf4g3#_45sd!kj',
+  jwtSecret: process.env.JWT_SECRET || '4Ihf4g3#_45sd!kj',
   salt: +process.env.SECRET_SALT || 12
 }
