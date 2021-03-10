8bd5d7302d274c1846da6cead95b9a4e5a0dc2c4
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-10 08:53:58 -0300

    feat: ensure Account returns an account on loadByEmail

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,11 +1,19 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helpers'
-export class AccountMongoRepository implements AddAccountRepository {
+
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const resultOperation = await accountCollection.insertOne(accountData)
     return MongoHelper.map(resultOperation.ops[0]) as AccountModel
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return MongoHelper.map(account) as AccountModel
   }
 }
