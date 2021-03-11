98dd064a852e27efb3ec9e7e95d1fa87408e9663
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-11 09:19:04 -0300

    feat: ensure SignupController calls Authenticator with correct values

--- src/presentation/controllers/singup/singup-controller.ts
+++ src/presentation/controllers/singup/singup-controller.ts
@@ -1,10 +1,11 @@
-import { Controller, HttpResponse, HttpRequest, AddAccount, Validator } from './singup-controller-protocols'
+import { Controller, HttpResponse, HttpRequest, AddAccount, Validator, Authenticator } from './singup-controller-protocols'
 import { badRequest, internalServerError, ok } from '../../helpers/http/http-helpers'
 
 export class SingUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validator: Validator
+    private readonly validator: Validator,
+    private readonly authenticator: Authenticator
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -14,7 +15,7 @@ export class SingUpController implements Controller {
 
       const { name, email, password } = httpRequest.body
       const account = await this.addAccount.add({ name, email, password })
-
+      await this.authenticator.auth({ email, password })
       return ok(account)
     } catch (error) {
       return internalServerError(error)
