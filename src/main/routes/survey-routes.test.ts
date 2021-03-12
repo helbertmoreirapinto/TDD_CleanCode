ea54c997025168743df52ef15c7d326239c8a227
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-12 09:33:37 -0300

    test: add AddSurveyComposition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,35 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
+import request from 'supertest'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  test('', async () => {
+    await request(app)
+      .post('/api/add-survey')
+      .send({
+        question: 'Question',
+        answers: [
+          { answer: 'Answer 1', image: 'Image 1' },
+          { answer: 'Answer 2' },
+          { answer: 'Answer 3', image: 'Image 3' }
+        ]
+      })
+      .expect(204)
+  })
+})
