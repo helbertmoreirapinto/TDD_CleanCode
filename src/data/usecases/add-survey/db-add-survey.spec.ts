e76d95e083b0b586ba0c56af74bee6775feb2315
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-12 07:11:58 -0300

    test: ensure DbAddSurvey throws AddSurveyRepository throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -45,4 +45,11 @@ describe('DbAddSurvey Usecases', () => {
       ]
     })
   })
+
+  test('Should throw if AddSurveyRepository throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.add(makeFakeAddSurvey())
+    await expect(promise).rejects.toThrow()
+  })
 })
