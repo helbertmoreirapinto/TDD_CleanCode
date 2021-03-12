28e2421b13855995c0277161256d972e57b8f2e9
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-12 20:27:58 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adater.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adater.spec.ts
@@ -31,19 +31,17 @@ describe('Jwt Adapter', () => {
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, env.jwtSecret)
     })
 
-    test('Should return access token on sign success', async () => {
+    test('Should throws if sign throws', async () => {
       const { sut } = makeSut()
-      const accessToken = await sut.encrypt('any_id')
-      expect(accessToken).toBe('any_token')
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const accessTokenPromise = sut.encrypt('any_id')
+      await expect(accessTokenPromise).rejects.toThrow()
     })
 
     test('Should return access token on sign success', async () => {
       const { sut } = makeSut()
-      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-        throw new Error()
-      })
-      const accessTokenPromise = sut.encrypt('any_id')
-      await expect(accessTokenPromise).rejects.toThrow()
+      const accessToken = await sut.encrypt('any_id')
+      expect(accessToken).toBe('any_token')
     })
   })
 
@@ -53,6 +51,13 @@ describe('Jwt Adapter', () => {
       const signSpy = jest.spyOn(jwt, 'verify')
       await sut.decrypt('any_token')
       expect(signSpy).toHaveBeenCalledWith('any_token', env.jwtSecret)
+    })
+
+    test('Should throws if verify throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
+      const accessTokenPromise = sut.decrypt('any_token')
+      await expect(accessTokenPromise).rejects.toThrow()
     })
   })
 })
