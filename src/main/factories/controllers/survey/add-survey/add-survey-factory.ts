6e28f23d504a96cb43d1718f9b24ae994ff28c4a
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-12 09:33:00 -0300

    feat: add AddSurveyComposition

---
+++ src/main/factories/controllers/survey/add-survey/add-survey-factory.ts
@@ -0,0 +1,10 @@
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { Controller } from '../../../../../presentation/protocols'
+import { AddSurveyController } from '../../../../../presentation/controllers/survey/add-survey/add-survey-controller'
+import { makeAddSurveyValidator } from './add-survey-validator-factory'
+import { makeDbAddSurvey } from '../../../usecases/add-survey/db-add-survey'
+
+export const makeAddSurveyController = (): Controller => {
+  const controller = new AddSurveyController(makeAddSurveyValidator(), makeDbAddSurvey())
+  return makeLogControllerDecorator(controller)
+}
