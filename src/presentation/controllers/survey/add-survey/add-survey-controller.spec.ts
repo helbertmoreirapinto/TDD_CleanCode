eb07fb2516f9c43ad5c6bbe8bd2f43c7316c79be
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-03-11 21:47:24 -0300

    test: ensure AddSurveyController calls AddSurvey with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,21 +1,31 @@
 import { AddSurveyController } from './add-survey-controller'
-import { HttpRequest, Validator } from './add-survey-controller-protocols'
+import { HttpRequest, Validator, AddSurvey, AddSurveyModel, SurveyModel } from './add-survey-controller-protocols'
 import { badRequest } from '../../../helpers/http/http-helpers'
 import { MissingParamError } from '../../../errors'
 
 interface SutTypes{
   sut: AddSurveyController
   validatorStub: Validator
+  addSurveyStub: AddSurvey
 }
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     question: 'any_question',
-    answers: [{
-      image: 'any_image',
-      answer: 'any_answer'
-    }]
+    answers: [
+      { image: 'any_image', answer: 'any_answer' },
+      { image: 'other_image', answer: 'other_answer' }
+    ]
   }
+})
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [
+    { image: 'any_image', answer: 'any_answer' },
+    { image: 'other_image', answer: 'other_answer' }
+  ]
 })
 
 const makeValidatorStub = (): Validator => {
@@ -27,13 +37,24 @@ const makeValidatorStub = (): Validator => {
   return new ValidatorStub()
 }
 
+const makeAddSurveyStub = (): any => {
+  class AddSurveyStub implements AddSurvey {
+    async add (survey: AddSurveyModel): Promise<SurveyModel> {
+      return await new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+  return new AddSurveyStub()
+}
+
 const makeSut = (): SutTypes => {
   const validatorStub = makeValidatorStub()
-  const sut = new AddSurveyController(validatorStub)
+  const addSurveyStub = makeAddSurveyStub()
+  const sut = new AddSurveyController(validatorStub, addSurveyStub)
 
   return {
     sut,
-    validatorStub
+    validatorStub,
+    addSurveyStub
   }
 }
 
@@ -51,5 +72,13 @@ describe('Add Survey Controller', () => {
     jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(new Promise(resolve => resolve(new MissingParamError('any_field'))))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('Should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut()
+    const addSpy = jest.spyOn(addSurveyStub, 'add')
+    const httpRequest: HttpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
